87d8a5bc41668fd18c485d769b84fdc9782cc6a5
Borano Llana <boranollana@Boranos-MacBook-Air.local>
2023-12-20 20:15:29 -0500

    =Preliminary TF try with criteria

--- shell/src/shell.rs
+++ shell/src/shell.rs
@@ -1,5 +1,5 @@
 pub(crate) mod utils;
-use abd_clam::{Cakes, PartitionCriteria, VecDataset};
+use abd_clam::{knn, rnn, Cakes, PartitionCriteria, VecDataset};
 
 #[allow(clippy::ptr_arg)]
 fn euclidean(x: &Vec<f32>, y: &Vec<f32>) -> f32 {
@@ -7,8 +7,8 @@ fn euclidean(x: &Vec<f32>, y: &Vec<f32>) -> f32 {
 }
 
 fn main() {
-    let seed = 42;
-    let (cardinality, dimensionality) = (1_000, 10);
+    let seed = 69;
+    let (cardinality, dimensionality) = (100, 10);
     let (min_val, max_val) = (-0.5, 0.5);
     let data: Vec<Vec<f32>> = symagen::random_data::random_tabular_seedable(
         cardinality,
@@ -20,6 +20,15 @@ fn main() {
 
     // We will generate some random labels for each point.
     let metadata: Vec<bool> = data.iter().map(|v| v[0] > 0.0).collect();
+
+    // We will use the origin as our query.
+    let query: Vec<f32> = vec![0.0; dimensionality];
+
+    // RNN search will use a radius of 0.05.
+    let radius: f32 = 0.5;
+
+    // KNN search will find the 10 nearest neighbors.
+    let k = 10;
 
     // The name of the dataset.
     let name = "demo".to_string();
@@ -49,9 +58,44 @@ fn main() {
 
     // The Cakes struct provides the functionality described in the CHESS paper.
     // We use a single shard here because the demo data is small.
-    let _model = Cakes::new(data, Some(seed), &criteria);
+    let model = Cakes::new(data, Some(seed), &criteria);
     // This line performs a non-trivial amount of work. #understatement
 
-    // TODO: This needs to be dropped because the data is reordered in Cakes.
-    drop(criteria);
+
+    // At this point, the dataset has been reordered to improve search performance.
+
+    // We can now perform RNN search on the model.
+    let rnn_results: Vec<(usize, f32)> =
+        model.rnn_search(&query, radius, rnn::Algorithm::default());
+
+    // We can also perform KNN search on the model.
+    let knn_results: Vec<(usize, f32)> = model.knn_search(&query, k, knn::Algorithm::default());
+
+    // Both results are a Vec of 2-tuples where the first element is the index of
+    // the point in the dataset and the second element is the distance from the
+    // query point.
+
+    // We can get the reordered metadata from the model.
+    let metadata: &[bool] = model.shards()[0].metadata().unwrap();
+
+    // Print how many trues and falses there are in the metadata
+    println!(
+        "Metadata: {:?} trues, {:?} falses",
+        metadata.iter().filter(|&&x| x).count(),
+        metadata.iter().filter(|&&x| !x).count()
+    );
+
+    // We can use the results to get the labels of the points that are within the
+    // radius of the query point.
+    let rnn_labels: Vec<bool> = rnn_results.iter().map(|&(i, _)| metadata[i]).collect();
+
+    // We can use the results to get the labels of the points that are the k nearest
+    // neighbors of the query point.
+    let knn_labels: Vec<bool> = knn_results.iter().map(|&(i, _)| metadata[i]).collect();
+
+    // Print the rnn labels
+    println!("RNN labels: {:?}", rnn_labels);
+
+    // Print the knn labels
+    println!("KNN labels: {:?}", knn_labels);
 }
